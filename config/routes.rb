68cb9bfefa202874c76440b63656e7d3ad1a8d8b
subaru-hello <octpsubaru@icloud.com>
2021-06-26 11:29:32 +0900

    rails-flavored-ruby

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
-
+  get  'static_pages/home'
+  get  'static_pages/help'
+  get  'static_pages/about'
+  get  'static_pages/contact'
   resources :static_pages
 end
