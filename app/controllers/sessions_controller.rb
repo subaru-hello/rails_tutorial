e467e315512d767b45d09dbf659bd6c15860632b
subaru-hello <octpsubaru@icloud.com>
2021-06-29 22:23:32 +0900

    基本ユーザー認証機能とページ保護機能の実装は完了

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,16 @@
 class SessionsController < ApplicationController
-  def new; end
+  def new
+  
+  end
 
   def create
-    @user = User.find_by(email: params[:session][:email].downcase)
-    if @user&.authenticate(params[:session][:password])
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
       #ユーザログイン後にユーザ情報のページヘリダイレクトする
-      log_in @user
+      log_in user
       #チェックされていたらユーザを記憶する
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_to @user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_back_or user
     else
       # エラーメッセージを作成する
       flash.now[:danger] = 'Invalid email/password combination'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   end
 
   def create
+    
     @user = User.new(user_params)
     if @user.save
       log_in @user
@@ -25,11 +26,11 @@ class UsersController < ApplicationController
   
 
   def edit
-    @user = User.find(params[:id])
+
   end
 
   def update
-    @user = User.find(params[:id])
+
     if @user.update(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -49,6 +50,7 @@ class UsersController < ApplicationController
     # ログイン済みユーザーかどうか確認
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
@@ -57,6 +59,6 @@ class UsersController < ApplicationController
     # 正しいユーザーかどうか確認
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url) unless @user == current_user
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
