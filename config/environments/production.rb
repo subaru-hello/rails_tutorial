0908e0b7a9d2aebb9f0ef2792f69ccf54eca9dd9
subaru-hello <octpsubaru@icloud.com>
2021-07-01 21:05:38 +0900

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,19 @@
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'infinite-temple-01469.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => host,
+    :authentication => :plain,
+  }
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
