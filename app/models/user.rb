5980c4406482c55cf10d64b32f0fc931290207ab
subaru-hello <octpsubaru@icloud.com>
2021-07-03 09:12:47 +0900

    マイクロポストを改良する

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@
 class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token, :reset_token
 
+  has_many :microposts, dependent: :destroy
+  default_scope -> { order(created_at: :desc) }
   before_save   :downcase_email
   before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
