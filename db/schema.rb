0a058dc6bf62aca9848d69e4a6e603c92748f753
subaru-hello <octpsubaru@icloud.com>
2021-07-03 18:01:14 +0900

    マイクロポストのアクセス制御

--- db/schema.rb
+++ db/schema.rb
@@ -10,31 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_702_130_117) do
-  create_table 'microposts', force: :cascade do |t|
-    t.text 'content'
-    t.integer 'user_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index %w[user_id created_at], name: 'index_microposts_on_user_id_and_created_at'
-    t.index ['user_id'], name: 'index_microposts_on_user_id'
+ActiveRecord::Schema.define(version: 2021_07_02_130117) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'email'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'password_digest'
-    t.string 'remember_digest'
-    t.boolean 'admin', default: false
-    t.string 'activation_digest'
-    t.boolean 'activated', default: false
-    t.datetime 'activated_at'
-    t.string 'reset_digest'
-    t.datetime 'reset_sent_at'
-    t.index ['email'], name: 'index_users_on_email', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.string "remember_digest"
+    t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  add_foreign_key 'microposts', 'users'
+  add_foreign_key "microposts", "users"
 end
