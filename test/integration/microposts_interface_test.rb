cd13fce19eb0a5632272fb4e075b43421e385ae1
subaru-hello <octpsubaru@icloud.com>
2021-07-04 21:40:16 +0900

    Add user microposts

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -1,29 +1,28 @@
 require 'test_helper'
 
 class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
-
   def setup
     @user = users(:michael)
   end
 
-  test "micropost interface" do
+  test 'micropost interface' do
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
     assert_select 'input[type=file]'
     # 無効な送信
     assert_no_difference 'Micropost.count' do
-      post microposts_path, params: { micropost: { content: "" } }
+      post microposts_path, params: { micropost: { content: '' } }
     end
     assert_select 'div#error_explanation'
-    assert_select 'a[href=?]', '/?page=2'  # 正しいページネーションリンク
+    assert_select 'a[href=?]', '/?page=2' # 正しいページネーションリンク
     # 有効な送信
-    content = "This micropost really ties the room together"
+    content = 'This micropost really ties the room together'
     picture = fixture_file_upload('test/fixtures/kitten.jpg', 'image/jpeg')
     assert_difference 'Micropost.count', 1 do
       post microposts_path, params: { micropost: { content: content, picture: picture } }
     end
-        # assert @micropost.image.attached?
+    # assert @micropost.image.attached?
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
@@ -37,7 +36,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     get user_path(users(:archer))
     assert_select 'a', text: 'delete', count: 0
   end
-  test "micropost sidebar count" do
+  test 'micropost sidebar count' do
     log_in_as(@user)
     get root_path
     assert_match "#{@user.microposts.count} microposts", response.body
@@ -45,9 +44,9 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     other_user = users(:malory)
     log_in_as(other_user)
     get root_path
-    assert_match "0 microposts", response.body
-    other_user.microposts.create!(content: "A micropost")
+    assert_match '0 microposts', response.body
+    other_user.microposts.create!(content: 'A micropost')
     get root_path
-    assert_match "1 micropost", response.body
+    assert_match '1 micropost', response.body
   end
 end
